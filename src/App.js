b5ecd7c9da46a6768036dbacb542e67ddd9448ac
jiapril11 <tadadadacoding@gmail.com>
2023-06-23 09:46:49 +0900

    Feat: input 기능

--- src/App.js
+++ src/App.js
@@ -1,33 +1,78 @@
+import { useEffect, useState } from "react";
 import Button from "./components/Button";
 import { FcLikePlaceholder, FcCloseUpMode } from "react-icons/fc";
 
 function App() {
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
+  const [item, setItem] = useState({});
+  const handleSubmitItem = (e) => {
+    e.preventDefault();
+    alert(`{name: '${name}', price: '${price.replace(",", "")}'}`);
+
+    setItem((prev) => ({ ...prev, name, price }));
+    setName("");
+    setPrice("");
+  };
+
   return (
     <>
       <div style={{ padding: 16 }}>
         <h1>Button</h1>
-        <div style={{ display: "flex", gap: "10px", marginBottom: "10px" }}>
-          <Button color="primary" size="lg" outline="true">
-            Large Button <FcLikePlaceholder />
-          </Button>
-          <Button color="primary" size="md">
-            Medium Button
-          </Button>
-          <Button color="primary" size="sm">
-            Small Button
-          </Button>
+        <div>
+          <div style={{ display: "flex", gap: "10px", marginBottom: "10px" }}>
+            <Button color="primary" size="lg" outline="true">
+              Large Button <FcLikePlaceholder />
+            </Button>
+            <Button color="primary" size="md">
+              Medium Button
+            </Button>
+            <Button color="primary" size="sm">
+              Small Button
+            </Button>
+          </div>
+          <div style={{ display: "flex", gap: "10px" }}>
+            <Button color="negativ" size="lg" outline="true">
+              Large Button <FcCloseUpMode />
+            </Button>
+            <Button color="negativ" size="md">
+              Medium Button
+            </Button>
+            <Button color="negativ" size="sm">
+              Small Button
+            </Button>
+          </div>
         </div>
-        <div style={{ display: "flex", gap: "10px" }}>
-          <Button color="negativ" size="lg" outline="true">
-            Large Button <FcCloseUpMode />
-          </Button>
-          <Button color="negativ" size="md">
-            Medium Button
-          </Button>
-          <Button color="negativ" size="sm">
-            Small Button
-          </Button>
-        </div>
+        <hr />
+        <h1>Input</h1>
+        <form onSubmit={handleSubmitItem}>
+          <div style={{ display: "flex" }}>
+            <div>
+              <label htmlFor="inputName">이름 </label>
+              <input
+                type="text"
+                id="inputName"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
+            </div>
+            <div>
+              <label htmlFor="inputPrice">가격 </label>
+              <input
+                type="text"
+                id="inputPrice"
+                value={price}
+                onInput={(e) =>
+                  (e.target.value = e.target.value
+                    .replace(/[^0-9]/g, "")
+                    .replace(/\B(?=(\d{3})+(?!\d))/g, ","))
+                }
+                onChange={(e) => setPrice(e.target.value)}
+              />
+            </div>
+            <button type="submit">저장</button>
+          </div>
+        </form>
       </div>
     </>
   );
